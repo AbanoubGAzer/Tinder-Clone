ecda282140741ef33dd717d161b4efbc6bfc855b
Ewerson Castelo <ewerson.castelo@gmail.com>
2019-08-07 20:36:38 -0400

    chore: final front end

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg'
 
 //componente sempre é uma função
-export default function Login(){
+export default function Login({ history }){ //todo componente que é rota herdam automaticamente o history
+  //username => obter valores 
+  //setUsername => modificar valores
+  const [ username, setUsername ] = useState('');
+
+    async function handleSubmit(event){
+      //previne o redirecionamento padrão para outra página
+      event.preventDefault();
+
+      const response = await api.post('/devs', {
+        username,
+      });
+
+      //pegar apena o id que veio dentro do response
+      const { _id } = response.data;
+
+      console.log(response);
+
+      //redireciona para respectiva página
+      history.push(`/dev/${_id}`);
+    }
+
     return (
       <div className="login-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <img src={logo} alt="Tindev" />
-          <input placeholder="Digite Seu Usuário do Github" />
+          <input 
+            placeholder="Digite Seu Usuário do Github"
+            value={username}
+            //valor digitado no input
+            onChange={event => setUsername(event.target.value)}
+            />
           <button type="submit">Enviar</button>
         </form>
-      </div>
+      </div> 
     );
 }
 
